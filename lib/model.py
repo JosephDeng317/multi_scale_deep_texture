f518d7868597779e5b324703a046a5cfe375af32
Joseph Deng <75818199+JosephDeng317@users.noreply.github.com>
2025-04-04 12:59:53 -0700

    upgraded to tensorflow2

--- lib/model.py
+++ lib/model.py
@@ -4,13 +4,9 @@ from __future__ import print_function
 
 import tensorflow as tf
 from lib.ops import *
-import collections
-import os
-import math
 from PIL import Image
 import numpy as np
-import time
-    
+
 # Define the dataloader
 def data_loader(FLAGS):
     with tf.device('/cpu:0'):
@@ -18,90 +14,92 @@ def data_loader(FLAGS):
         if FLAGS.texture_shape == [-1,-1]:
             image_raw = check_size(image_raw)
         else:
-            image_raw = image_raw.resize( (FLAGS.texture_shape[0], FLAGS.texture_shape[1]) )
-        if image_raw.mode is not 'RGB':
+            image_raw = image_raw.resize((FLAGS.texture_shape[0], FLAGS.texture_shape[1]))
+        if image_raw.mode != 'RGB':
             image_raw = image_raw.convert('RGB')
-        image_raw = np.asarray(image_raw)/255
-        targets = preprocess(image_raw)  
-        samples = np.expand_dims(targets, axis = 0) 
+        image_raw = np.asarray(image_raw) / 255.0
+        targets = preprocess(image_raw)
+        samples = np.expand_dims(targets, axis=0)
     return samples
 
-def generator(FLAGS, target, init = None):
+def generator(FLAGS, target, init=None):
     if init is not None:
-        var = tf.Variable(init + tf.random_normal(tf.shape(init), 0, FLAGS.stddev))  
+        var = tf.Variable(init + tf.compat.v1.random_normal(tf.shape(init), mean=0.0, stddev=FLAGS.stddev))
     else:
         if FLAGS.texture_shape == [-1,-1]:
-            shape = [1, target.shape[1],target.shape[2],3]
+            shape = [1, target.shape[1], target.shape[2], 3]
         else:
-            shape = [1, FLAGS.texture_shape[0],FLAGS.texture_shape[1],3]
-        var = tf.get_variable('gen_img',shape = shape, \
-                  initializer = tf.random_normal_initializer(0,0.5), \
-                               dtype=tf.float32,trainable=True, collections=None)   
+            shape = [1, FLAGS.texture_shape[0], FLAGS.texture_shape[1], 3]
+        var = tf.compat.v1.get_variable('gen_img', shape=shape,
+                  initializer=tf.random_normal_initializer(mean=0.0, stddev=0.5),
+                  dtype=tf.float32, trainable=True)
     return tf.tanh(var)
 
 def Synthesis(initials, targets, upsampling, FLAGS):
-    with tf.variable_scope('generator'):
-        if initials is None:
-            pass
-        else:
-            w, h = [ initials.shape[1], initials.shape[2] ]
+    with tf.compat.v1.variable_scope('generator', reuse=tf.compat.v1.AUTO_REUSE):
+        if initials is not None:
+            w, h = int(initials.shape[1]), int(initials.shape[2])
             try:
                 initials = tf.constant(initials)
             except:
                 pass
-            if upsampling == True:
-                    initials = tf.image.resize_bicubic(initials, [2*int(w), 2*int(h)])
+            if upsampling:
+                # Replace tf.image.resize_bicubic with tf.image.resize using BICUBIC method
+                initials = tf.image.resize(initials, [2 * w, 2 * h], method=tf.image.ResizeMethod.BICUBIC)
         gen_output = generator(FLAGS, targets, initials)
 
-        # Calculating the generator loss
-    with tf.name_scope('generator_loss'):   
+    # Calculating the generator loss
+    with tf.name_scope('generator_loss'):
         with tf.name_scope('tv_loss'):
             tv_loss = total_variation_loss(gen_output)
-
         with tf.name_scope('style_loss'):
-            _, vgg_gen_output = vgg_19(gen_output,is_training=False, reuse = False)
-            _, vgg_tar_output = vgg_19(targets,   is_training=False, reuse = True)
-            style_layer_list = get_layer_list(FLAGS.top_style_layer,False)
-            sl = tf.zeros([])
-            ratio_list=[100.0, 1.0, 0.1, 0.0001, 1.0, 100.0]
+            _, vgg_gen_output = vgg_19(gen_output, is_training=False, reuse=tf.compat.v1.AUTO_REUSE)
+            _, vgg_tar_output = vgg_19(targets, is_training=False, reuse=tf.compat.v1.AUTO_REUSE)
+            style_layer_list = get_layer_list(FLAGS.top_style_layer, False)
+            sl = tf.zeros([], dtype=tf.float32)
+            ratio_list = [100.0, 1.0, 0.1, 0.0001, 1.0, 100.0]
             for i in range(len(style_layer_list)):
                 tar_layer = style_layer_list[i]
                 target_layer = get_layer_scope(tar_layer)
                 gen_feature = vgg_gen_output[target_layer]
                 tar_feature = vgg_tar_output[target_layer]
                 diff = tf.square(gram(gen_feature) - gram(tar_feature))
-                sl = sl + tf.reduce_mean(tf.reduce_sum(diff, axis=0)) * ratio_list[i] 
+                sl = sl + tf.reduce_mean(tf.reduce_sum(diff, axis=0)) * ratio_list[i]
             style_loss = sl
-                
+
         gen_loss = style_loss + FLAGS.W_tv * tv_loss
         gen_loss = 1e6 * gen_loss
 
     with tf.name_scope('generator_train'):
-        gen_tvars = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, scope = 'generator')
-        optimizer = tf.contrib.opt.ScipyOptimizerInterface(
-            gen_loss, var_list = gen_tvars, method='L-BFGS-B',
-            options = {'maxiter': FLAGS.max_iter, 'disp': FLAGS.print_loss})
+        gen_tvars = tf.compat.v1.get_collection(tf.compat.v1.GraphKeys.TRAINABLE_VARIABLES, scope='generator')
+        # Replace the ScipyOptimizerInterface with AdamOptimizer in TF2
+        optimizer = tf.compat.v1.train.AdamOptimizer(learning_rate=0.01)
+        train_op = optimizer.minimize(gen_loss, var_list=gen_tvars)
 
-    vgg_var_list = tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES, scope='vgg_19')
-    vgg_restore = tf.train.Saver(vgg_var_list)
+    # Get list of VGG variables and create saver for restoring
+    vgg_var_list = tf.compat.v1.get_collection(tf.compat.v1.GraphKeys.GLOBAL_VARIABLES, scope='vgg_19')
+    vgg_restore = tf.compat.v1.train.Saver(vgg_var_list)
 
     # Start the session
-    config = tf.ConfigProto()
+    config = tf.compat.v1.ConfigProto()
     config.gpu_options.allow_growth = True
 
-    def print_loss(gl, sl, tvl):
-        if FLAGS.print_loss is True:
-            print('gen_loss : %s' % gl )
-            print('style_loss : %s' % sl )
-            print('tv_loss : %s' % tvl )
-    
-    init_op = tf.global_variables_initializer()   
-    with tf.Session(config = config) as sess:
+    def print_loss(gl, sl_val, tvl):
+        if FLAGS.print_loss:
+            print('gen_loss : %s' % gl)
+            print('style_loss : %s' % sl_val)
+            print('tv_loss : %s' % tvl)
+
+    init_op = tf.compat.v1.global_variables_initializer()
+    with tf.compat.v1.Session(config=config) as sess:
         sess.run(init_op)
         vgg_restore.restore(sess, FLAGS.vgg_ckpt)
         print('Under Synthesizing ...')
-        #start = time.time()
-        optimizer.minimize(sess, loss_callback = print_loss,
-                         fetches = [gen_loss, style_loss, tv_loss])
 
-        return gen_output.eval(), style_loss.eval()
+        # Run training loop for max_iter steps
+        for i in range(FLAGS.max_iter):
+            gl, sl_val, tvl, _ = sess.run([gen_loss, style_loss, tv_loss, train_op])
+            print_loss(gl, sl_val, tvl)
+
+        gen_out, style_loss_out = sess.run([gen_output, style_loss])
+        return gen_out, style_loss_out
--- main.py
+++ main.py
@@ -3,94 +3,91 @@ from __future__ import division
 from __future__ import print_function
 
 import tensorflow as tf
-import tensorflow.contrib.slim as slim
+tf.compat.v1.disable_eager_execution()
+
 import os
-from lib.model import data_loader, generator, Synthesis
+from lib.model import data_loader, Synthesis
 from lib.ops import *
-import math
 import time
 import numpy as np
 import scipy.misc
-import cv2
 import argparse
-os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2' 
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 
 parser = argparse.ArgumentParser()
 
 parser.add_argument(
     '--output_dir',
-    help = 'output dictionary',
-    default = './result/'
+    help='output dictionary',
+    default='./result/'
 )
 
 parser.add_argument(
     '--vgg_ckpt',
-    help = 'checkpoint of vgg networks, the check point file of pretrained model should be downloaded',
-    default = '/home/liaoqian/DATA/vgg19/vgg_19.ckpt'
+    help='checkpoint of vgg networks, the check point file of pretrained model should be downloaded',
+    default='./vgg19/vgg_19.ckpt'
 )
 
 parser.add_argument(
     '--target_dir',
-    help = 'path of target img, texture sample image or style image',
-    default = './imgs/tomato.png' 
+    help='path of target img, texture sample image or style image',
+    default='./imgs/tomato.png'
 )
 
 parser.add_argument(
     '--initials',
-    help = 'initialized mode of synthesis, come into force only in style_transfer task_mode',
-    choices = ['noise', 'content'],
-    default = 'noise'
+    help='initialized mode of synthesis, come into force only in style_transfer task_mode',
+    choices=['noise', 'content'],
+    default='noise'
 )
 
 parser.add_argument(
     '--top_style_layer',
-    help = 'the top layer of vgg network layers used to compute style_loss',
-    default = 'VGG54',
-    choices = ['VGG11','VGG21','VGG31','VGG41','VGG51','VGG54']
+    help='the top layer of vgg network layers used to compute style_loss',
+    default='VGG54',
+    choices=['VGG11','VGG21','VGG31','VGG41','VGG51','VGG54']
 )
 
 parser.add_argument(
     '--texture_shape',
-    help = 'img_size of synthesis output texture, if set to [-1,-1], the shape will be \
-    the same as sample texture image',
-    nargs = '+',
-    type = int
+    help='img_size of synthesis output texture, if set to [-1,-1], the shape will be the same as sample texture image',
+    nargs='+',
+    type=int
 )
 
 parser.add_argument(
     '--pyrm_layers',
-    help = 'layers number of pyramid',
-    default = 6,
-    type = int
+    help='layers number of pyramid',
+    default=6,
+    type=int
 )
 
 parser.add_argument(
     '--W_tv',
-    help = 'weight of total variation loss',
-    type = float,
-    default = 0.1
+    help='weight of total variation loss',
+    type=float,
+    default=0.1
 )
 
 parser.add_argument(
     '--stddev',
-    help = 'standard deviation of noise',
-    type = float,
-    default = 0.1
+    help='standard deviation of noise',
+    type=float,
+    default=0.1
 )
-
 
 parser.add_argument(
     '--max_iter',
-    help = 'max iteration',
-    type = int,
-    default = 100,
-    required = True
+    help='max iteration',
+    type=int,
+    default=100,
+    required=True
 )
 
 parser.add_argument(
     '--print_loss',
-    help = 'whether print current loss',
-    action = 'store_true'
+    help='whether print current loss',
+    action='store_true'
 )
 
 FLAGS = parser.parse_args()
@@ -104,39 +101,42 @@ if FLAGS.output_dir is None:
 if not os.path.exists(FLAGS.output_dir):
     os.mkdir(FLAGS.output_dir)
 
-# pyramid = get_pyramid(targets, pyramid_layers) # a list store the pyramid of target textures image
 pyrm_layers = FLAGS.pyrm_layers
 start = time.time()
-tar_name = FLAGS.target_dir.split('/')[-1]; tar_name = tar_name.split('.')[0]
+tar_name = FLAGS.target_dir.split('/')[-1]
+tar_name = tar_name.split('.')[0]
 targets0 = data_loader(FLAGS)
 
-for i in range(pyrm_layers - 1, -1, -1) :
-    targets = tf.constant(targets0, dtype = tf.float32)
-    w0, h0 = [targets.shape[1], targets.shape[2]]; w, h = [ w0//(2**i), h0//(2**i) ]
-    target = tf.image.resize_bicubic(targets, [w, h])
-    print('\nCurrent image : ',tar_name)
-    print('Image size : ',target.shape)
-    print('Now in pyramid layer %i, total %i layer (from L%i to L0)\n'%(i, pyrm_layers, pyrm_layers-1))
-    if i == pyrm_layers - 1: # if now initial the large scale gen_output 
+for i in range(pyrm_layers - 1, -1, -1):
+    targets = tf.constant(targets0, dtype=tf.float32)
+    w0, h0 = targets.shape[1], targets.shape[2]
+    w, h = w0 // (2**i), h0 // (2**i)
+    # Using tf.image.resize with bicubic method
+    target = tf.image.resize(targets, [w, h], method=tf.image.ResizeMethod.BICUBIC)
+    print('\nCurrent image : ', tar_name)
+    print('Image size : ', target.shape)
+    print('Now in pyramid layer %i, total %i layer (from L%i to L0)\n' % (i, pyrm_layers, pyrm_layers-1))
+    if i == pyrm_layers - 1:  # initialize large scale gen_output
         if FLAGS.texture_shape == [-1, -1]:
             FLAGS.texture_shape = [w, h]
-        else: 
-            FLAGS.texture_shape = [ FLAGS.texture_shape[0]//(2**i), \
-                                    FLAGS.texture_shape[1]//(2**i)]
+        else:
+            FLAGS.texture_shape = [FLAGS.texture_shape[0] // (2**i),
+                                     FLAGS.texture_shape[1] // (2**i)]
         try:
-            gen_output, style_loss = Synthesis(None, target,\
-                                   upsampling = False, FLAGS = FLAGS)     
+            gen_output, style_loss = Synthesis(None, target,
+                                               upsampling=False, FLAGS=FLAGS)
         except:
-            raise ValueError('Pyramid is too higher ! ')
+            raise ValueError('Pyramid is too higher!')
     else:
-        gen_output, style_loss = Synthesis(gen_output, target,\
-                                       upsampling = True, FLAGS = FLAGS) 
+        gen_output, style_loss = Synthesis(gen_output, target,
+                                           upsampling=True, FLAGS=FLAGS)
     if i == 0:
         total_time = (time.time() - start)
-        scipy.misc.toimage(np.squeeze(gen_output), cmin=-1.0, cmax=1.0) \
-                        .save(os.path.join(FLAGS.output_dir,'%s_%i_%i_%i_%.1f.png'%
-                                           (tar_name, pyrm_layers, \
-                                            gen_output.shape[1],gen_output.shape[2],total_time)))
-    tf.reset_default_graph()  
-        
-print('Optimization done !!! ') 
+        scipy.misc.toimage(np.squeeze(gen_output), cmin=-1.0, cmax=1.0).save(
+            os.path.join(FLAGS.output_dir, '%s_%i_%i_%i_%.1f.png' % (tar_name, pyrm_layers,
+                                                                       gen_output.shape[1],
+                                                                       gen_output.shape[2],
+                                                                       total_time)))
+    # tf.reset_default_graph() removed in TF2
+
+print('Optimization done !!!')
